49e99b7beccf6542edae79639ce646c5c4324bdb
krobawsky <richibq20@gmail.com>
2017-09-20 12:55:07 -0500

    working......

--- app/src/main/java/com/tecsup/paginasamarillasapp/Activity/MainActivity.java
+++ app/src/main/java/com/tecsup/paginasamarillasapp/Activity/MainActivity.java
@@ -2,14 +2,26 @@ package com.tecsup.paginasamarillasapp.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 import com.tecsup.paginasamarillasapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AutoCompleteTextView autoComplete;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String [] texts = {"Restaurants","Boticas","Grifos","Colegios","Rentas","Remates","Mercados","Autos","Universidades"};
+
+        autoComplete = (AutoCompleteTextView) findViewById(R.id.autoComplete);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,texts);
+        autoComplete.setAdapter(adapter);
+
     }
 }
