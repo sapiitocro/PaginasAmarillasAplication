ba1c362b735f8a5c0143af752ecd02f9a374f8a0
krobawsky <richibq20@gmail.com>
2017-09-28 13:49:03 -0500

    Semi Finish ...!!!

--- app/src/main/java/com/tecsup/paginasamarillasapp/Detalles/Detalles.java
+++ app/src/main/java/com/tecsup/paginasamarillasapp/Detalles/Detalles.java
@@ -1,16 +1,35 @@
 package com.tecsup.paginasamarillasapp.Detalles;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.tecsup.paginasamarillasapp.Adaptador.Adaptador;
+import com.tecsup.paginasamarillasapp.Datos.Datos;
 import com.tecsup.paginasamarillasapp.R;
 
+import java.util.ArrayList;
+
 public class Detalles extends AppCompatActivity {
+
+    private TextView j1,j2,j3,jj1;
+    private ImageView j4;
 
     private TextView p1;
 
@@ -19,37 +38,135 @@ public class Detalles extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles);
 
+        j1 = (TextView) findViewById(R.id.j1);
+        jj1 = (TextView) findViewById(R.id.jj1);
+        j2 = (TextView) findViewById(R.id.j2);
+        j3 = (TextView) findViewById(R.id.j3);
+        j4 = (ImageView) findViewById(R.id.j4);
+
         Bundle bb = getIntent().getExtras();
-        String posicion=bb.getString("cartilla");
+        String posicion = bb.getString("cartilla");
 
-        p1 = (TextView) findViewById(R.id.p1);
-        p1.setText(posicion);
+        if(posicion.equals("0")){
+            j1.setText("POLLERIA ROKY´S");
+            j2.setText("av. Lima # 123");
+            j3.setText("7630057");
+            j4.setImageResource(R.mipmap.logo_pollo);
+            jj1.setText("LOS MEJORES POLLOS A LA BRASA DEL PERÚ");
+        }else if (posicion.equals("1")){
+            j1.setText("HAMBURGUESAS");
+            j2.setText("av. Pirata # 6074");
+            j3.setText("7502598");
+            j4.setImageResource(R.mipmap.logo_hamburguesa);
+            jj1.setText("SI DESEAS COMER LAS MEJORES HAMBURGUESAS DEL PERÚ, BEMBOS ES LA BUENA ELECCIÓN");
+        }else if(posicion.equals("2")){
+            j1.setText("PIZZA´S");
+            j2.setText("av. Peru # 505");
+            j3.setText("7201030");
+            j4.setImageResource(R.mipmap.logo_pizza1);
+            jj1.setText("LAS PIZZAS CON MAS SABOR, EN PIZZA´S DEBES COMER");
+        }else if(posicion.equals("3")){
+            j1.setText("KFC");
+            j2.setText("av. La Revolución # 1069");
+            j3.setText("7205093");
+            j4.setImageResource(R.mipmap.logo_kfc1);
+            jj1.setText("KFC , EL MEJOR LUGAR PARA LA FAMILIA PERUANA");
+        }else if(posicion.equals("4")){
+            j1.setText("CHIFA");
+            j2.setText("av. La marina # 8080");
+            j3.setText("7500187");
+            j4.setImageResource(R.mipmap.logo_chifa1);
+            jj1.setText("LOS PLATILLOS MAS DELICIOSOS DEL ORIENTE, LO TIENES EN UN SOLO LUGAR");
+        }else if(posicion.equals("5")){
+            j1.setText("TURRONES SAN JOSE");
+            j2.setText("av. La Cruz # 23");
+            j3.setText("7566501");
+            j4.setImageResource(R.mipmap.logo_turrones1);
+            jj1.setText("TURRONES SAN JOSE, SUAVECIIIIIITOOSSS.....");
+        }else if(posicion.equals("6")){
+            j1.setText("CAFE EL DICHO");
+            j2.setText("av. Ugarte # 101");
+            j3.setText("7100098");
+            j4.setImageResource(R.mipmap.logo_cafe1);
+            jj1.setText("TODO CAFE CARGADO, BIENE CON UN DICHO RECARGADO");
+        }else if(posicion.equals("7")){
+            j1.setText("MC DONALDS");
+            j2.setText("av. Argentina # 3489");
+            j3.setText("7025378");
+            j4.setImageResource(R.mipmap.logo_mc);
+            jj1.setText("MC DONALDS EL MEJOR LUGAR PARA DISFRUTAR EN FAMILIA");
+        }else if(posicion.equals("8")){
+            j1.setText("PARDOS");
+            j2.setText("av. Benavides # 3902");
+            j3.setText("7198356");
+            j4.setImageResource(R.mipmap.logo_pardos1);
+            jj1.setText("POLLERIA PARDOS CUENTA CON LOS MEJORES POLLOS PARA SU MESA");
+        }else if(posicion.equals("9")){
+            j1.setText("CAFE 338");
+            j2.setText("av. Colectora # 7514");
+            j3.setText("75000522");
+            j4.setImageResource(R.mipmap.logo_cafe11);
+            jj1.setText("CAFE , ANIS , LECHE ; ...  TODO EN UN SOLO LUGAR... TE ESPERAMOS ....");
+        }
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navegation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.menu_home:
-                        Toast.makeText(Detalles.this, "Go home section...", Toast.LENGTH_SHORT).show();
+
+                switch (item.getItemId()){
+
+                    case R.id.action_navegar:
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse("http://www.google.com"));
+                        startActivity(intent);
                         break;
-                    case R.id.menu_camera:
-                        Toast.makeText(Detalles.this, "Go camera section...", Toast.LENGTH_SHORT).show();
+
+                    case R.id.action_enviarCorreo:
+
+                        Intent i = new Intent(Intent.ACTION_SEND);
+                        i.setType("message/rfc822");
+                        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"recipient@example.com"});
+                        i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+                        i.putExtra(Intent.EXTRA_TEXT, "body of email");
+                        try {
+                            startActivity(Intent.createChooser(i, "Send mail..."));
+                        } catch (android.content.ActivityNotFoundException ex) {
+                            Toast.makeText(getApplicationContext(), "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                        }
                         break;
-                    case R.id.menu_search:
-                        Toast.makeText(Detalles.this, "Go share section...", Toast.LENGTH_SHORT).show();
+                    case R.id.action_enviarMensaje:
+
+                        Uri uri = Uri.parse("smsto:"+997138919);
+                        Intent it = new Intent(Intent.ACTION_SENDTO, uri);
+                        it.putExtra("sms_body", "hola");
+                        startActivity(it);
                         break;
-                    case R.id.menu_notification:
-                        Toast.makeText(Detalles.this, "Go notification section...", Toast.LENGTH_SHORT).show();
+                    case R.id.action_compartir:
+
+                        Intent intenta = new Intent(Intent.ACTION_SEND);
+                        intenta.setType("text/plain");
+                        intenta.putExtra(Intent.EXTRA_SUBJECT, "subject");
+                        intenta.putExtra(Intent.EXTRA_TEXT, "text");
+                        intenta.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(Intent.createChooser(intenta,  "Compartir en" ));
                         break;
-                    case R.id.menu_more:
-                        Toast.makeText(Detalles.this, "Go more sections...", Toast.LENGTH_SHORT).show();
+                    case R.id.action_llamar:
+                        int permissionCheck = ContextCompat.checkSelfPermission(Detalles.this, Manifest.permission.CALL_PHONE);
+
+                        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(
+                                    Detalles.this,
+                                    new String[]{Manifest.permission.CALL_PHONE},
+                                    Integer.parseInt("123"));
+                        } else {
+                            startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:"+ 997138910)));
+                        }
                         break;
                 }
                 return true;
             }
         });
-
     }
-
 }
--- app/src/main/java/com/tecsup/paginasamarillasapp/Resultados/Resultados.java
+++ app/src/main/java/com/tecsup/paginasamarillasapp/Resultados/Resultados.java
@@ -1,6 +1,8 @@
 package com.tecsup.paginasamarillasapp.Resultados;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -45,17 +47,20 @@ public class Resultados extends AppCompatActivity {
 
             barra.setText("0 resultados de "+dato);
         }
-
-
-
-
-
     }
 
     public void cargarLista(){
 
         arrayItem.add(new Datos("POLLERIA ROKY´S","av. Lima # 123","7630057",R.mipmap.logo_pollo));
         arrayItem.add(new Datos("HAMBURGUESAS","av. Pirata # 6074","7502598",R.mipmap.logo_hamburguesa));
+        arrayItem.add(new Datos("PIZZA´S","av. Peru # 505","7201030",R.mipmap.logo_pizza1));
+        arrayItem.add(new Datos("KFC","av. La Revolución # 1069","7205093",R.mipmap.logo_kfc1));
+        arrayItem.add(new Datos("CHIFA","av. La marina # 8080","7500187",R.mipmap.logo_chifa1));
+        arrayItem.add(new Datos("TURRONES SAN JOSE","av. La Cruz # 23","7566501",R.mipmap.logo_turrones1));
+        arrayItem.add(new Datos("CAFE EL DICHO","av. Ugarte # 101","7100098",R.mipmap.logo_cafe1));
+        arrayItem.add(new Datos("MC DONALDS","av. Argentina # 3489","7025378",R.mipmap.logo_mc));
+        arrayItem.add(new Datos("PARDOS","av. Benavides # 3902","7198356",R.mipmap.logo_pardos1));
+        arrayItem.add(new Datos("CAFE 338","av. Colectora # 7514","75000522",R.mipmap.logo_cafe11));
 
 
         adapter = new Adaptador(this, arrayItem);
@@ -75,5 +80,7 @@ public class Resultados extends AppCompatActivity {
         });
 
     }
+
+
 
 }
